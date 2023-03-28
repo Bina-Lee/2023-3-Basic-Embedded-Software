8fb24b90cf38223b39d5e2897b82128792ba4395
이진성 <awwdde22ljs@gmail.com>
2023-03-28 21:17:28 +0900

    week4

---
+++ week4.py
@@ -0,0 +1,14 @@
+# operator operand expression
+# ** : 제곱을 반환
+# /  : 그냥 나누기 실수형
+# // : 몫을 반환
+# %  : 나머지를 반환
+
+# 복합대입연산자
+# x+=2 == x=x+2
+
+# 우선순위
+# a=b=c : b=c, a=b/a=c
+
+math.pow(3,2)
+math.sqrt(4)
